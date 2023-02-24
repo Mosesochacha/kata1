c9550b36903e21157ade4365968b6678b23c2997
mosesochacha <ochachamoses443@@gmail.com>
2023-02-24 09:31:52 +0300

    done

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+// pseudocode
+// convert the number to a string
+// split the numbers to get each value
+// map through an array of strings
+// square the each value in array
+// Change the  values into interger
+// join the numbers
+
+var number = 5656
 const prompt = require('prompt-sync')();
-let number = prompt("ENTER THE NUMBER : ")
+var  number = prompt("ENTER THE NUMBER : ")
 function newArray() {
   let newnumber = number.toString().split("");
+  console.log();
   let num = newnumber.map((num) => {let newnum = Math.pow(num, 2);
+    console.log(newnum);
     return newnum;
+    
   });
   let newnumarr =parseInt(num.join(""));
   console.log(newnumarr);
